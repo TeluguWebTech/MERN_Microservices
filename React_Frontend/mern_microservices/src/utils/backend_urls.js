eb31036cb1366af758ff4cbed654c472fc6b5266
teluguwebtech <kalyan.vtz@gmail.com>
2026-05-11 13:29:59 +0530

    aws_url_updated

--- React_Frontend/mern_microservices/src/utils/backend_urls.js
+++ React_Frontend/mern_microservices/src/utils/backend_urls.js
@@ -5,4 +5,5 @@ export const cartUrl = ""
 export const emailUrl = ""
 export const orderUrl = ""
 
-export const baseUrl = "http://localhost:9000/api"
+// export const baseUrl = "http://localhost:9000/api"
+export const baseUrl = "http://65.0.107.71:9000/api"
