4650e8d7a8b87e4a1c25bb0bc395a1f62ea06462
teluguwebtech <kalyan.vtz@gmail.com>
2026-05-11 14:06:56 +0530

    dockerized microservices deployment setup

--- notification-service/kafka/client.js
+++ notification-service/kafka/client.js
@@ -2,7 +2,8 @@ const {Kafka} = require("kafkajs")
 
 const kafka = new Kafka({
     clientId:"notification-service",
-    brokers: ["127.0.0.1:9092"]
+    // brokers: ["127.0.0.1:9092"]
+    brokers:["kafka:9092"]
 });
 
 module.exports = kafka;
